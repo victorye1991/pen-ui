6847b9769b4cd32bf4fac279eb5842e10ba12755
gabe.johnson <gabe.johnson@265e9650-9819-0410-a30a-67447f70629e>
2009-11-24 20:08:54 +0000

    Adding some things... this is not a complete commit, and the repository is unstable.

---
+++ slippy/src/org/six11/olive/Main.java
@@ -0,0 +1,47 @@
+// $Id$
+
+package org.six11.olive;
+
+import org.six11.util.args.Arguments;
+import org.six11.util.args.Arguments.ArgType;
+import org.six11.util.args.Arguments.ValueType;
+import org.six11.util.gui.ApplicationFrame;
+
+public class Main {
+  
+  public static final String VERSION = "$Id$";
+  
+  public static void main(String[] in) {
+    ApplicationFrame af = new ApplicationFrame("Olive");
+    af.setSize(800, 600);
+    af.center();
+
+    Arguments args = new Arguments();
+    args.addFlag("load-path", ArgType.ARG_REQUIRED, ValueType.VALUE_REQUIRED,
+        "Specifies the root load path for Slippy code.");
+    args.addFlag("help", ArgType.ARG_OPTIONAL, ValueType.VALUE_IGNORED, "Shows documentation.");
+    args.addFlag("v", ArgType.ARG_OPTIONAL, ValueType.VALUE_IGNORED, "Display version.");
+    args.parseArguments(in);
+    if (args.hasFlag("help")) {
+      System.out.println(args.getDocumentation());
+      System.exit(0);
+    }
+    if (args.hasFlag("v")) {
+      System.out.println(VERSION);
+      System.exit(0);
+    }
+    try {
+      args.validate();
+    } catch (IllegalArgumentException ex) {
+      System.out.println(args.getUsage());
+      System.exit(-1);
+    }
+
+    String loadPath = args.hasValue("load-path") ? args.getValue("load-path") : ".";
+
+    OliveApplet applet = new OliveApplet(false, loadPath);
+    applet.init();
+    af.add(applet);
+    af.setVisible(true);
+  }
+}
