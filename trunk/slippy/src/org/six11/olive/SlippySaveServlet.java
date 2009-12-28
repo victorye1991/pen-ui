d23e38688367dfe5731bf6f0af02f5f1d1468ca6
gabe.johnson@gmail.com <gabe.johnson@gmail.com@265e9650-9819-0410-a30a-67447f70629e>
2009-12-28 20:39:00 +0000

    Adding the 'save' servlet.

---
+++ trunk/slippy/src/org/six11/olive/SlippySaveServlet.java
@@ -0,0 +1,56 @@
+package org.six11.olive;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.six11.util.Debug;
+import org.six11.util.io.FileUtil;
+
+/**
+ * 
+ * 
+ * @author Gabe Johnson <johnsogg@cmu.edu>
+ */
+public class SlippySaveServlet extends HttpServlet {
+  
+  public final static String CACHE_DIR_PARAM = "jarVendorCacheDir"; // must agree with web.xml
+  public final static String MODULE_DIR_PARAM = "moduleDir"; // must agree with web.xml
+//  public final static String ORIGINAL_JAR_PARAM = "originalJar"; // must agree with web.xml
+
+
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+      IOException {
+    String module = req.getParameter("module");
+    String who = req.getParameter("who");
+    String src = req.getParameter("source");
+    String fqClass = req.getParameter("fqClass");    
+    File baseDir = getModuleDir();
+    String lowerPath = SlippyBundler.getPathFragment(module, "working", who);
+    File path = new File(baseDir, lowerPath);
+    FileUtil.complainIfNotWriteable(path);
+    DiskEnvironment env = new DiskEnvironment(path.getAbsolutePath());
+    env.save(fqClass, src);
+    resp.setStatus(HttpServletResponse.SC_OK);
+    resp.getOutputStream().close();
+    bug("Saved " + who + "'s " + module + "/" + fqClass + " (" + src.length() + " bytes)");
+  }
+  
+  protected File getCacheDir() {
+    String dir = getInitParameter(CACHE_DIR_PARAM);
+    return new File(dir);
+  }
+
+  protected File getModuleDir() {
+    String dir = getInitParameter(MODULE_DIR_PARAM);
+    return new File(dir);
+  }
+  
+  private static void bug(String what) {
+    Debug.out("SlippySaveServlet", what);
+  }
+}
