ac60b3034b5c08835de740df98815b49d97c0404
gabe.johnson <gabe.johnson@gmail.com>
2010-03-06 00:12:06 +0000

    Progress! Still not done, but closer. Most of the tricky stuff is now out of the way. The hard part was deciphering the code enough to understand what my datastructures should be. But by the time I figured it out I had committed to something else that wasn't quite as good. So I've been porting my old integer-indexing strategy to a new Node-int approach. This makes it easier to keep track which values go with what.

--- skrui/src/org/six11/skrui/bayes/BayesianNetwork.java
+++ skrui/src/org/six11/skrui/bayes/BayesianNetwork.java
@@ -1,15 +1,19 @@
 package org.six11.skrui.bayes;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
 import org.six11.util.Debug;
 
 /**
- * This code was translated from 'markov.py' by Marcus Frean and Tony Vignaux.
+ * I manually decrypted the singly-connected belief propagation algorithm found in
+ * "Learning Bayesian Networks" by Richard Neapolitan and have attempted to reconstruct it in Java.
+ * It probably contains gigantic errors and oversights.
  * 
  * @author Gabe Johnson <johnsogg@cmu.edu>
  */
@@ -21,9 +25,21 @@ public class BayesianNetwork {
     Debug.useColor = false;
     Debug.useTime = false;
     Debug.setDecimalOutputFormat("0.000");
-//    BayesianNetwork net = BayesianNetwork.makeSimpleNetwork();
+    // BayesianNetwork net = BayesianNetwork.makeSimpleNetwork();
     BayesianNetwork net = BayesianNetwork.makeNeapolitanExample310();
+    // net.mondoDebug(true);
     net.initializeNetwork();
+  }
+
+  private void mondoDebug(boolean initializeNodes) {
+    if (initializeNodes) {
+      for (Node n : nodes) {
+        n.initializePropagationSlots();
+      }
+    }
+    for (Node n : nodes) {
+      n.mondoDebug();
+    }
   }
 
   private static void bug(String what) {
@@ -52,46 +68,114 @@ public class BayesianNetwork {
   }
 
   List<Node> nodes;
+  List<Node> activatedNodes;
+//  Map<Node, Integer> activatedStates;
+  List<SlotKey> activatedStates;
 
   public BayesianNetwork() {
     nodes = new ArrayList<Node>();
+    activatedNodes = new ArrayList<Node>();
+    activatedStates = new ArrayList<SlotKey>();
   }
-  
+
   public void initializeNetwork() {
+    System.out.println("Clearing activated nodes/states.");
+    activatedNodes.clear();
+    activatedStates.clear();
+    
+    System.out.println("Initializing propagation slots.");
     for (Node n : nodes) {
       n.initializePropagationSlots();
     }
+    
+    System.out.println("Initializing child val/msg and parent messages.");
     for (Node n : nodes) {
-      for (int i=0; i < n.getStateCount(); i++) {
+      for (int i = 0; i < n.getStateCount(); i++) {
         n.setChildValue(i, 1); // Compute lambda values
       }
       for (Node z : n.parents) {
-        for (int i=0; i < z.getStateCount(); i++) {
+        for (int i = 0; i < z.getStateCount(); i++) {
           z.setChildMessage(n, i, 1); // Compute lambda messages
         }
       }
       for (Node y : n.children) {
-        for (int i=0; i < n.getStateCount(); i++) {
+        for (int i = 0; i < n.getStateCount(); i++) {
           n.setParentMessage(y, i, 1); // Initialize pi messages
         }
       }
-      Set<Node> roots = getRoots(nodes);
-      for (Node r : roots) {
-        for (int i=0; i < r.getStateCount(); i++) {
-          // P(r|a) = P(r)
-          // pi(r) = P(r)
-        }
-        for (Node x : r.children) {
-          // send_pi_msg(r, x)
-        }
+    }
+    
+    Set<Node> roots = getRoots(nodes);
+    System.out.println("Setting parent values on " + roots.size() + " roots.");
+    for (Node r : roots) {
+      for (int i = 0; i < r.getStateCount(); i++) {
+        r.setParentValue(i, r.getProbabilityVector()[i]); // Compute R's pi values.
+      }
+      for (Node x : r.children) {
+        sendParentMessage(r, x);
       }
     }
-    
-    for (Node n : nodes) {
-      n.mondoDebug();
-    }
+    mondoDebug(false);
   }
-    
+
+  private void sendParentMessage(Node z, Node x) {
+    for (int i = 0; i < z.getStateCount(); i++) {
+      double prod = 1;
+      for (Node y : z.children) {
+        if (y != x) {
+          prod = prod * z.getChildMessage(y)[i];
+        }
+      }
+      prod = prod * z.getParentValue(i);
+      z.setParentMessage(x, i, prod); // Z sends X a pi message
+    }
+
+    if (!activatedNodes.contains(x)) {
+      SlotKey[][] combos = x.cpt.getParentIndexCombinations();
+      for (int i = 0; i < x.getStateCount(); i++) {
+        // This involves getting the probability of every combination of X's parents. In the
+        // burglary example, for a1, this is:
+        // ````````` term1 ````````` term2 ```` term3 `` (there may be more Pi_* terms)
+        // pi(a1) = P(a1 | b1,f1) * Pi_A(b1) * Pi_A(f1) + // line 0
+        // ```````` P(a1 | b1,f2) * Pi_A(b1) * Pi_A(f2) + // line 1
+        // ```````` P(a1 | b2,f1) * Pi_A(b2) * Pi_A(f1) + // line 2
+        // ```````` P(a1 | b2,f2) * Pi_A(b2) * Pi_A(f2) ; // line 3
+        // 
+        // ... and similarly for a2.
+        System.out.print("pi(" + x.cpt.getStateName(i) + ") = ");
+        double[] lines = new double[combos.length];
+        for (int j = 0; j < combos.length; j++) {
+          SlotKey[] parentValueSlots = combos[j];
+          double[] myCptData = x.cpt.getData(parentValueSlots);
+          double term1 = myCptData[i];
+          System.out.print(" + (" + Debug.num(term1) + ") ");
+          double[] fromParents = new double[x.parents.size()];
+          for (int k = 0; k < x.parents.size(); k++) {
+            Node par = x.parents.get(k);
+            int parIdx = parentValueSlots[k].index;
+            fromParents[k] = par.getParentMessage(x)[parIdx];
+            System.out.print("(" + fromParents[k] + ") ");
+          }
+          double prod = term1;
+          for (int k = 0; k < fromParents.length; k++) {
+            prod = prod * fromParents[k];
+          }
+          lines[j] = prod;
+        }
+        double sum = 0;
+        for (int j = 0; j < lines.length; j++) {
+          sum = sum + lines[j];
+        }
+        System.out.println(" = " + Debug.num(sum));
+        x.setParentValue(i, sum);
+        x.setInferredBelief(i, activatedStates, x.getChildValue(i) * x.getParentValue(i));
+      }
+      double[] norm = normalize(x.getInferredBelief(activatedStates));
+      x.setInferredBelief(activatedStates, norm);
+    }
+
+  }
+
   private Set<Node> getRoots(List<Node> all) {
     Set<Node> ret = new HashSet<Node>();
     for (Node n : all) {
@@ -111,10 +195,10 @@ public class BayesianNetwork {
     }); // No qualifier necessary because clouds has no parent.
     rain.cpt.setRow(new double[] {
         0.01, 0.99
-    }, 0); // Qualifier 0 refers to parent's slot-0 variable: Sunny
+    }, clouds.slotKey(0)); // Qualifier 0 refers to parent's slot-0 variable: Sunny
     rain.cpt.setRow(new double[] {
         0.3, 0.7
-    }, 1); // Qualifier 1 refers to parent's slot-1 variable: Cloudy
+    }, clouds.slotKey(1)); // Qualifier 1 refers to parent's slot-1 variable: Cloudy
     BayesianNetwork net = new BayesianNetwork();
     net.addNode(clouds);
     net.addNode(rain);
@@ -128,33 +212,38 @@ public class BayesianNetwork {
     Node b = new Node("Burglary", "b1", "b2");
     Node f = new Node("Freight Truck", "f1", "f2");
     Node a = new Node("Alarm", "a1", "a2");
-    Node.link(b, a);
     Node.link(f, a);
-    b.cpt.setRow(new double[] {
-       0.005, 0.995 
+    Node.link(b, a);
+    b.setProbabilityDistribution(new double[] {
+        0.005, 0.995
     });
-    f.cpt.setRow(new double[] {
-        0.03, 0.97 
-     });
-    a.cpt.setRow(new double[] {
-        0.992, 1-0.992
-    }, 0, 0);
-    a.cpt.setRow(new double[] {
-        0.99, 1-0.99
-    }, 1, 0);
-    a.cpt.setRow(new double[] {
-        0.2, 1-0.2
-    }, 0, 1);
-    a.cpt.setRow(new double[] {
-        0.003, 1-0.003
-    }, 1, 1);
+    f.setProbabilityDistribution(new double[] {
+        0.03, 0.97
+    });
+
+    a.setProbabilityDistribution(new double[] { // b1, f1
+            0.992, 1 - 0.992
+        }, b.slotKey(0), f.slotKey(0));
+
+    a.setProbabilityDistribution(new double[] { // b1, f2
+            0.99, 1 - 0.99
+        }, b.slotKey(0), f.slotKey(1));
+
+    a.setProbabilityDistribution(new double[] { // b2, f1
+            0.2, 1 - 0.2
+        }, b.slotKey(1), f.slotKey(0));
+
+    a.setProbabilityDistribution(new double[] { // b2, f2
+            0.003, 1 - 0.003
+        }, b.slotKey(1), f.slotKey(1));
+
     BayesianNetwork net = new BayesianNetwork();
     net.addNode(b);
     net.addNode(f);
     net.addNode(a);
     return net;
   }
-  
+
   public void addNode(Node n) {
     nodes.add(n);
   }
