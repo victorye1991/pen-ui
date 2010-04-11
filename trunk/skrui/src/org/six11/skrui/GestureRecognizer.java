f9e1bc03088d9ff462e523fb5d07677035ff60b4
gabe.johnson@gmail.com <gabe.johnson@gmail.com@265e9650-9819-0410-a30a-67447f70629e>
2010-04-11 15:54:51 +0000

    Moved many debugging functions (including vestigial ones) to a new DebugUtil class. Also moved the pen hover and structured-ink drawing functions to the GestureRecognizer script. Neanderthal is now basically just a hub for various scripts, and holds data common to them.

--- trunk/skrui/src/org/six11/skrui/GestureRecognizer.java
+++ trunk/skrui/src/org/six11/skrui/GestureRecognizer.java
@@ -1,15 +1,19 @@
 package org.six11.skrui;
 
+import java.awt.Color;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.six11.skrui.script.Neanderthal;
+import org.six11.skrui.shape.LineSegment;
 import org.six11.skrui.shape.Primitive;
 import org.six11.util.Debug;
 import org.six11.util.args.Arguments;
 import org.six11.util.args.Arguments.ArgType;
 import org.six11.util.args.Arguments.ValueType;
+import org.six11.util.pen.*;
 
 /**
  * This gesture recognizer looks for time-ordered sequences of input that satisfy constraints. The
@@ -24,9 +28,12 @@ import org.six11.util.args.Arguments.ValueType;
  * 
  * @author Gabe Johnson <johnsogg@cmu.edu>
  */
-public class GestureRecognizer extends SkruiScript {
+public class GestureRecognizer extends SkruiScript implements SequenceListener, HoverListener {
 
   GestureRecognizerDomain grDomain;
+  Neanderthal data;
+  HoverEvent lastHoverEvent;
+
 
   @SuppressWarnings("unused")
   private String listPrims(Set<Primitive> set) {
@@ -40,13 +47,83 @@ public class GestureRecognizer extends SkruiScript {
   private static void bug(String what) {
     Debug.out("GestureRecognizer", what);
   }
+  
+  private void hideStructuredInk() {
+    if (getSoup().getBuffer("structured") != null) {
+      getSoup().getBuffer("structured").setVisible(false);
+      main.getDrawingSurface().repaint();
+    }
+  }
+
+  public static double calcAlpha(double dist) {
+    double minAlpha = 0.05;
+    double maxAlpha = 0.6;
+    double minDist = 10;
+    double maxDist = 150;
+    double ret = 0;
+    if (dist < minDist) {
+      ret = maxAlpha;
+    } else if (dist > maxDist) {
+      ret = minAlpha;
+    } else {
+      ret = maxAlpha - ((dist - minDist) / (maxDist - minDist)) * (maxAlpha - minAlpha);
+    }
+    return ret;
+  }
+  
+  private void drawStructuredInk() {
+    DrawingBuffer db = new DrawingBuffer();
+    Pt pen = lastHoverEvent.getPt();
+    // Color debugColor = new Color(1f, 0.3f, 0.3f, 1f);
+    for (Pt st : data.getStructurePoints().getPoints()) {
+      double dist = st.distance(pen);
+      double alpha = calcAlpha(dist);
+      Color c = new Color(0.6f, 0.6f, 0.6f, (float) alpha);
+      DrawingBufferRoutines.cross(db, st, 3.5, c);
+    }
+    for (LineSegment line : data.getStructureLines()) {
+      double dist = Functions.getDistanceBetweenPointAndLine(pen, line.getGeometryLine());
+      double alpha = calcAlpha(dist);
+      Color c = new Color(0.6f, 0.6f, 0.6f, (float) alpha);
+      DrawingBufferRoutines.screenLine(db, main.getDrawingSurface().getBounds(), line
+          .getGeometryLine(), c, 0.7);
+    }
+    List<Pt> rec = data.getStructurePoints().getRecent(30000, pen.getTime());
+    Color veryLightGray = new Color(0.6f, 0.6f, 0.6f, (float) 0.3);
+    if (rec.size() > 0) {
+      // draw a line from pen to the most recent point
+      Pt st = rec.get(0);
+      DrawingBufferRoutines.line(db, pen, st, veryLightGray, 0.5);
+    }
+    for (int i = 0; i < rec.size() - 1; i++) {
+      // draw lines connecting each dot to the previous
+      Pt here = rec.get(i);
+      Pt there = rec.get(i + 1);
+      Line ghostLine = new Line(here, there);
+      Pt mid = ghostLine.getMidpoint();
+      DrawingBufferRoutines.line(db, ghostLine, veryLightGray, 0.5);
+      DrawingBufferRoutines.cross(db, mid, 2.3, veryLightGray);
+    }
+    if (rec.size() > 2) {
+      // when there are three points, make a circle and show its center.
+      Pt center = Functions.getCircleCenter(rec.get(0), rec.get(1), rec.get(2));
+      if (center != null) { // if 0-1-2 are colinear, center is null. avoid NPEs
+        double radius = rec.get(0).distance(center);
+        DrawingBufferRoutines.dot(db, center, radius, 0.5, veryLightGray, null);
+        DrawingBufferRoutines.dot(db, center, 3.0, 0.3, veryLightGray, veryLightGray);
+      }
+    }
+    getSoup().addBuffer("structured", db);
+  }
 
   @Override
   public void initialize() {
     bug("Initializing...");
-    Neanderthal data = (Neanderthal) main.getScript("Neanderthal");
+    data = (Neanderthal) main.getScript("Neanderthal");
     grDomain = new GestureRecognizerDomain(data);
     data.addPrimitiveListener(grDomain);
+    data.addSequenceListener(this);
+    main.getDrawingSurface().getSoup().addHoverListener(this);
     bug("Initialized. Bring it on, world.");
   }
 
@@ -84,8 +161,46 @@ public class GestureRecognizer extends SkruiScript {
     return args;
   }
 
+  public void handleHoverEvent(HoverEvent hoverEvent) {
+    lastHoverEvent = hoverEvent;
+    whackStructuredInk();
+  }
+
+  
   public static Map<String, BoundedParameter> getDefaultParameters() {
     Map<String, BoundedParameter> defs = new HashMap<String, BoundedParameter>();
     return defs;
+  }
+
+  /**
+   * When the pen moves around inside the drawing area, we want structural ink to be redrawn as a
+   * function of what the user is doing, and where the pen is. Consult 'lastHoverEvent' to know
+   * what's going on.
+   */
+  private void whackStructuredInk() {
+    if (data.getStructurePoints().size() > 0) {
+      if (lastHoverEvent != null) {
+        switch (lastHoverEvent.getType()) {
+          case In:
+          case Hover:
+            drawStructuredInk();
+            break;
+          case Out:
+            hideStructuredInk();
+            break;
+        }
+      }
+    }
+  }
+  public void handleSequenceEvent(SequenceEvent seqEvent) {
+    SequenceEvent.Type type = seqEvent.getType();
+    Sequence seq = seqEvent.getSeq();
+    switch (type) {
+      case PROGRESS:
+        if (lastHoverEvent != null) {
+          lastHoverEvent.setPt(seq.getLast());
+          whackStructuredInk();
+        }
+    }
   }
 }
---
+++ trunk/skrui/src/org/six11/skrui/script/DebugUtil.java
@@ -0,0 +1,229 @@
+package org.six11.skrui.script;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+
+import org.six11.skrui.script.Neanderthal.Certainty;
+import org.six11.skrui.shape.LineSegment;
+import org.six11.skrui.shape.Primitive;
+import org.six11.util.Debug;
+import org.six11.util.pen.DrawingBuffer;
+import org.six11.util.pen.Functions;
+import org.six11.util.pen.Pt;
+import org.six11.util.pen.Sequence;
+import org.six11.skrui.DrawingBufferRoutines;
+import org.six11.skrui.Main;
+
+/**
+ * Assorted debugging functions that aren't strictly needed for other classes to work. This might be
+ * something of a code graveyard, but at least it is all in one spot.
+ * 
+ * @author Gabe Johnson <johnsogg@cmu.edu>
+ */
+public class DebugUtil {
+
+  Main main;
+
+  DebugUtil(Main main) {
+    this.main = main;
+  }
+
+  void output(String... stuff) {
+    boolean first = true;
+    for (String v : stuff) {
+      if (!first) {
+        System.out.print(", ");
+      }
+      first = false;
+      System.out.print(v);
+    }
+    System.out.print("\n");
+  }
+
+  void output(double... stuff) {
+    boolean first = true;
+    for (double v : stuff) {
+      if (!first) {
+        System.out.print(", ");
+      }
+      first = false;
+      System.out.print(Debug.num(v));
+    }
+    System.out.print("\n");
+  }
+
+  int sumCertainty(Certainty... certainties) {
+    int sum = 0;
+    for (Certainty c : certainties) {
+      switch (c) {
+        case Maybe:
+        case Yes:
+          sum += 1;
+      }
+    }
+    return sum;
+  }
+
+  void drawDots(List<Pt> spots, String bufferName) {
+    if (spots.size() > 1) {
+      DrawingBuffer db = main.getDrawingSurface().getSoup().getBuffer(bufferName);
+      if (db == null) {
+        db = new DrawingBuffer();
+        main.getDrawingSurface().getSoup().addBuffer(bufferName, db);
+      }
+      DrawingBufferRoutines.dots(db, spots, 2.5, 0.3, Color.BLACK, Color.PINK);
+    }
+  }
+
+  void drawDots(Sequence seq, boolean plain, boolean curvy, boolean slow, boolean both,
+      boolean corner, String bufferName) {
+    if (seq.size() > 1) {
+      DrawingBuffer db = main.getDrawingSurface().getSoup().getBuffer(bufferName);
+      if (db == null) {
+        db = new DrawingBuffer();
+        main.getDrawingSurface().getSoup().addBuffer(bufferName, db);
+        db.setVisible(false);
+      }
+      List<Pt> plainDots = new ArrayList<Pt>();
+      List<Pt> curvyDots = new ArrayList<Pt>();
+      List<Pt> slowDots = new ArrayList<Pt>();
+      List<Pt> bothDots = new ArrayList<Pt>();
+      List<Pt> cornerDots = new ArrayList<Pt>();
+      for (Pt pt : seq) {
+        if (plain) {
+          plainDots.add(pt);
+        }
+        if (curvy && pt.getBoolean("curvy")) {
+          curvyDots.add(pt);
+        }
+        if (slow && pt.getBoolean("slow")) {
+          slowDots.add(pt);
+        }
+        if (both && pt.getBoolean("both")) {
+          bothDots.add(pt);
+        }
+        if (corner && pt.getBoolean("corner")) {
+          cornerDots.add(pt);
+        }
+      }
+      if (plainDots.size() > 0) {
+        DrawingBufferRoutines.dots(db, plainDots, 2.5, 0.3, Color.BLACK, null);
+      }
+      if (curvyDots.size() > 0) {
+        DrawingBufferRoutines.dots(db, curvyDots, 2.5, 0.3, Color.BLACK, Color.YELLOW);
+      }
+      if (slowDots.size() > 0) {
+        DrawingBufferRoutines.dots(db, slowDots, 2.5, 0.3, Color.BLACK, Color.BLUE);
+      }
+      if (bothDots.size() > 0) {
+        DrawingBufferRoutines.dots(db, bothDots, 2.5, 0.3, Color.BLACK, Color.GREEN);
+      }
+      if (cornerDots.size() > 0) {
+        DrawingBufferRoutines.dots(db, cornerDots, 2.5, 0.3, Color.BLACK, Color.RED);
+      }
+    }
+  }
+
+  void drawPrims(Set<Primitive> inSet, Color color, String name) {
+    DrawingBuffer db = new DrawingBuffer();
+    boolean dirty = false;
+    for (Primitive prim : inSet) {
+      if (prim instanceof LineSegment) {
+        dirty = true;
+        DrawingBufferRoutines.patch(db, prim.getSeq(), prim.getStartIdx(), prim.getEndIdx(), 2.0,
+            color);
+        DrawingBufferRoutines.text(db, Functions.getMean(prim.getP1(), prim.getP2()).getTranslated(
+            5, 10), prim.getShortStr(), Color.BLACK);
+      }
+    }
+    if (dirty) {
+      main.getDrawingSurface().getSoup().addBuffer(name, db);
+    } else {
+      main.getDrawingSurface().getSoup().removeBuffer(name);
+    }
+  }
+
+  void drawSimilarLength(Sequence seq) {
+    DrawingBuffer db = new DrawingBuffer();
+    boolean dirty = false;
+    Neanderthal data = (Neanderthal) main.getScript("Neanderthal");
+    for (Primitive prim : Neanderthal.getPrimitiveSet(seq)) {
+      dirty = true;
+      DrawingBufferRoutines.patch(db, seq, prim.getStartIdx(), prim.getEndIdx(), 2.0, Color.RED);
+      double pathLength = prim.getSeq().getPathLength(prim.getStartIdx(), prim.getEndIdx());
+      Set<Primitive> similar = data.getLengthGraph().getSimilar(pathLength, pathLength * 0.25);
+      for (Primitive adj : similar) {
+        if ((adj.getSeq() == prim.getSeq() && adj.getStartIdx() == prim.getStartIdx() && adj
+            .getEndIdx() == prim.getEndIdx()) == false) {
+          DrawingBufferRoutines.patch(db, adj.getSeq(), adj.getStartIdx(), adj.getEndIdx(), 2.0,
+              Color.GREEN);
+        }
+      }
+    }
+    if (dirty) {
+      main.getDrawingSurface().getSoup().addBuffer("similength", db);
+    } else {
+      main.getDrawingSurface().getSoup().removeBuffer("similength");
+    }
+  }
+
+  void drawAdjacent(Sequence seq) {
+    DrawingBuffer db = new DrawingBuffer();
+    boolean dirty = false;
+    Neanderthal data = (Neanderthal) main.getScript("Neanderthal");
+    for (Primitive prim : Neanderthal.getPrimitiveSet(seq)) {
+      dirty = true;
+      // DrawingBufferRoutines.patch(db, seq, prim.getStartIdx(), prim.getEndIdx(), 2.0, Color.RED);
+      double pathLength = prim.getSeq().getPathLength(prim.getStartIdx(), prim.getEndIdx());
+      double radius = pathLength * 0.20;
+      Set<Pt> ends = data.getEndPoints().getNear(prim.getSeq().get(prim.getStartIdx()), radius);
+      ends.addAll(data.getEndPoints().getNear(prim.getSeq().get(prim.getEndIdx()), radius));
+      Set<Primitive> adjacentPrims = Neanderthal.getPrimitives(ends);
+      adjacentPrims.remove(prim);
+      for (Primitive adj : adjacentPrims) {
+        DrawingBufferRoutines.patch(db, adj.getSeq(), adj.getStartIdx(), adj.getEndIdx(), 2.0,
+            Color.GREEN);
+      }
+    }
+    if (dirty) {
+      main.getDrawingSurface().getSoup().addBuffer("adjacent", db);
+    } else {
+      main.getDrawingSurface().getSoup().removeBuffer("adjacent");
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  void drawParallelPerpendicular(Sequence seq) {
+    DrawingBuffer db = new DrawingBuffer();
+    boolean dirty = false;
+    Neanderthal data = (Neanderthal) main.getScript("Neanderthal");
+    for (Primitive prim : (SortedSet<Primitive>) seq.getAttribute(Neanderthal.PRIMITIVES)) {
+      if (prim instanceof LineSegment) {
+        dirty = true;
+        DrawingBufferRoutines.patch(db, seq, prim.getStartIdx(), prim.getEndIdx(), 2.0, Color.RED);
+        Set<Primitive> set = data.getAngleGraph().getNear((LineSegment) prim, 0, 0.2);
+        // bug(set.size() + " are parallel to " + ((LineSegment) prim).getFixedAngle() + ": ");
+        for (Primitive p : set) {
+          // bug("  " + p.getFixedAngle());
+          DrawingBufferRoutines.patch(db, p.getSeq(), p.getStartIdx(), p.getEndIdx(), 2.0,
+              Color.BLUE);
+        }
+        set = data.getAngleGraph().getNear((LineSegment) prim, Math.PI / 2, 0.2);
+        // bug(set.size() + " are perpendicular to " + ((LineSegment) prim).getFixedAngle() + ": ");
+        for (Primitive p : set) {
+          // bug("  " + p.getFixedAngle());
+          DrawingBufferRoutines.patch(db, p.getSeq(), p.getStartIdx(), p.getEndIdx(), 2.0,
+              Color.GREEN);
+        }
+      }
+    }
+    if (dirty) {
+      main.getDrawingSurface().getSoup().addBuffer("paraperp", db);
+    } else {
+      main.getDrawingSurface().getSoup().removeBuffer("paraperp");
+    }
+  }
+}
