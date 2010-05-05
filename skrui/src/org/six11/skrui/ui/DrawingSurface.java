b1658c53a81d80639a0c6a0128b863b6d7b1eaa9
gabe.johnson <gabe.johnson@gmail.com>
2010-05-05 00:07:07 +0000

    Improved the widgets atop the screen. In particular the ColorSquare now leaves a trail of the color you've mixed in the right thickness. And it has a neat fading effect over time.

--- skrui/src/org/six11/skrui/ui/DrawingSurface.java
+++ skrui/src/org/six11/skrui/ui/DrawingSurface.java
@@ -6,7 +6,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
-import java.util.Collections;
 import java.util.List;
 
 import javax.swing.JComponent;
