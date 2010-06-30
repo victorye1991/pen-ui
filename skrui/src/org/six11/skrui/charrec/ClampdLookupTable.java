28a5724fbf323822d58947677fa728ff927424c3
gabe.johnson <gabe.johnson@gmail.com>
2010-06-30 14:59:36 +0000

    moving to another machine. this might not even compile.

--- skrui/src/org/six11/skrui/charrec/ClampdLookupTable.java
+++ skrui/src/org/six11/skrui/charrec/ClampdLookupTable.java
@@ -1,5 +1,9 @@
 package org.six11.skrui.charrec;
 
+import java.util.Random;
+
+import org.six11.util.Debug;
+
 /**
  * A simple lookup table that lets you quickly convert two-byte sequences to doubles in the range
  * [0..1]. Obviously this only has 16 bits of precision, but dividing the range [0..1] into 65,536
@@ -16,25 +20,34 @@ public class ClampdLookupTable {
 
   public static void main(String[] args) {
     ClampdLookupTable clam = new ClampdLookupTable();
-    for (int i = 0; i < args.length; i = i + 2) {
-      try {
-        byte a = Byte.parseByte(args[i]);
-        int ai = 128 + a;
-        byte b = Byte.parseByte(args[i + 1]);
-        int bi = 128 + b;
-        int idx = ai * 256 + bi;
-        double v = clam.get(idx);
-        System.out.println("bytes " + a + " and " + b + " indicate the integers " + ai + " and "
-            + bi);
-        System.out.println("Unsigned short " + idx + " = " + v);
-        System.out.println("Reversing that, the index of " + v + " is: "
-            + ClampdLookupTable.getIndexOf(v));
-      } catch (NumberFormatException ex) {
-        ex.printStackTrace();
-      }
+    int LOTS = 1000000;
+    // try it two ways: first, do not use a lookup table, but get the value of 1,000,000 int
+    // indices. second, use the lookup table to get the value of 1,000,000 int indices
+    Random rand = new Random(System.currentTimeMillis());
+    int[] index = new int[LOTS];
+    for (int i=0; i < index.length; i++) {
+      index[i] = rand.nextInt(boundary);
     }
+    long startA = System.nanoTime();
+    for (int i=0; i < index.length; i++) {
+      // convert index[i] from the 0-2^16 space to 0..1 double-space
+      double result = (double) index[i] / boundary;
+//      bug(index[i] + " --> " + Debug.num(result));
+    }
+    long endA = System.nanoTime();
+    long startB = System.nanoTime();
+    for (int i=0; i < index.length; i++) {
+      clam.get(index[i]);
+    }
+    long endB = System.nanoTime();
+    bug("Elapsed time (calculating every time): " + (endA - startA));
+    bug("Elapsed time (using lookup table):     " + (endB - startB));
   }
 
+  private static void bug(String what) {
+    Debug.out("ClampdLookupTable", what);
+  }
+  
   public ClampdLookupTable() {
     table = new double[boundary]; // 65536 * 8 bytes = half a meg of memory. just so you know.
     for (int bytes = 0; bytes < boundary; bytes++) {
