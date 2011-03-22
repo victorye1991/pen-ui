d086c76058cbb47621d482a000b18cd2e6211fa2
gabe.johnson@gmail.com <gabe.johnson@gmail.com@265e9650-9819-0410-a30a-67447f70629e>
2011-03-22 02:16:48 +0000

    Old color picker thing, probably will never use this again

--- trunk/color-picker/src/org/six11/colorpicker/Main.java
+++ trunk/color-picker/src/org/six11/colorpicker/Main.java
@@ -1,10 +1,38 @@
 package org.six11.colorpicker;
 
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JWindow;
+
+import org.six11.util.Debug;
+import org.six11.util.gui.ApplicationFrame;
+import org.six11.util.gui.Components;
+
 /**
  * 
  **/
 public class Main {
   public static void main(String[] args) {
-    // test
+    ApplicationFrame af = new ApplicationFrame("Sample Sketching Area");
+    af.setSize(800, 600);
+    af.center();
+    af.setVisible(true);
+
+    JFrame window = new JFrame();
+    window.setUndecorated(true);
+    ColorPicker cp = new ColorPicker();
+    window.add(cp);
+    window.setSize(201,201);
+    window.setAlwaysOnTop(true);
+    Components.centerComponent(window);
+    window.setVisible(true);
+    
+    bug("You should see something now.");
   }
+
+  private static void bug(String what) {
+    Debug.out("Main", what);
+  }
+  
+  
 }
