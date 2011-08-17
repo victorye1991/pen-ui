b1a1a05e32c134da74b76f65393b86596da120aa
gabe.johnson <gabe.johnson@gmail.com>
2011-08-17 20:24:28 +0000

    UI shows up and has placeholders

--- thesis-code/src/org/six11/sf/Main.java
+++ thesis-code/src/org/six11/sf/Main.java
@@ -3,8 +3,10 @@ package org.six11.sf;
 import static org.six11.util.Debug.bug;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.six11.util.gui.ApplicationFrame;
@@ -14,30 +16,46 @@ import org.six11.util.gui.ApplicationFrame;
  **/
 public class Main {
 
-  static Map<String, Object> vars;
+  /**
+   * holds general info about the application (e.g. when it was run). Useful for debugging and
+   * documentation.
+   */
+  Map<String, Object> vars;
 
+  /**
+   * Creates a new SkruiFab application and shows a single editor window.
+   */
   public static void main(String[] args) {
-    initVars();
-    ApplicationFrame af = new ApplicationFrame("SkruiFab (build " + varStr("dateString") + " at "
-        + varStr("timeString") + ")");
-    af.setSize(600, 400);
-    
-    af.center();
-    af.setVisible(true);
+    new Main().go();
   }
   
-  private static void initVars() {
+  /**
+   * Initialize variables, but does not show any GUI elements.
+   */
+  public Main() {
     vars = new HashMap<String, Object>();
     vars.put("date", new Date());
     vars.put("dateString", new SimpleDateFormat("MMM dd yyyy").format(varDate("date")));
     vars.put("timeString", new SimpleDateFormat("K:mm a").format(varDate("date")));
+    vars.put("editors", new ArrayList<SkruiFabEditor>());
+    
   }
 
-  public static String varStr(String key) {
+  /**
+   * Creates and shows a single new editor window.
+   */
+  public void go() {
+    SkruiFabEditor ed = new SkruiFabEditor(this);
+    List<SkruiFabEditor> editors = (List<SkruiFabEditor>) vars.get("editors");
+    editors.add(ed);
+  }
+
+
+  public String varStr(String key) {
     return (String) vars.get(key);
   }
 
-  public static Date varDate(String key) {
+  public Date varDate(String key) {
     return (Date) vars.get(key);
   }
 }
---
+++ thesis-code/src/org/six11/sf/SkruiFabEditor.java
@@ -0,0 +1,62 @@
+package org.six11.sf;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+import org.six11.util.gui.ApplicationFrame;
+import org.six11.util.layout.FrontEnd;
+import org.six11.util.pen.PenEvent;
+import org.six11.util.pen.PenListener;
+import static org.six11.util.Debug.bug;
+import static org.six11.util.layout.FrontEnd.*;
+
+/**
+ * A self-contained editor instance.
+ * 
+ * @author Gabe Johnson <johnsogg@cmu.edu>
+ */
+public class SkruiFabEditor implements PenListener {
+
+  Main main;
+  DrawingBufferLayers layers;
+  
+  public SkruiFabEditor(Main m) {
+    this.main = m;
+    ApplicationFrame af = new ApplicationFrame("SkruiFab (started " + m.varStr("dateString") + " at "
+        + m.varStr("timeString") + ")");
+    af.setSize(600, 400);
+    layers = new DrawingBufferLayers();
+    layers.addPenListener(this);
+
+    JPanel grid = new JPanel();
+    grid.setBackground(Color.BLUE);
+    JPanel cutfile = new JPanel();
+    cutfile.setBackground(Color.RED);
+    cutfile.setPreferredSize(new Dimension(300, 200));
+    FrontEnd fe = new FrontEnd();
+    fe.add(layers, "layers");
+    fe.add(grid, "grid");
+    fe.add(cutfile, "cutfile");
+    fe.addRule(ROOT, N, "layers", N);
+    fe.addRule(ROOT, W, "layers", W);
+    fe.addRule(ROOT, S, "layers", S);
+    fe.addRule(ROOT, N, "grid", N);
+    fe.addRule(ROOT, E, "grid", E);
+    fe.addRule(ROOT, E, "cutfile", E);
+    fe.addRule(ROOT, S, "cutfile", S);
+    fe.addRule("cutfile", N, "grid", S);
+    fe.addRule("cutfile", W, "layers", E);
+    fe.addRule("cutfile", W, "grid", W);
+    af.add(fe);
+    af.center();
+    af.setVisible(true);
+  }
+
+  public void handlePenEvent(PenEvent ev) {
+    bug("method not implemented yet!");
+  }
+
+}
